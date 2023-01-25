f5d17ae3a6aefa7d54fd2d221389a3b717a977f5
devpatidar22 <122958375+devpatidar22@users.noreply.github.com>
2023-01-25 00:08:48 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -4,5 +4,3 @@ print("There should be one-- and preferably only one --obvious way to do it.")
 
 #This lines very offensive to dutch people
 
-
-print ("Although that way may not be obvious at first unless you're Dutch.")
--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@ print("Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
 #KISS = Keep it simple, Stupid
 print(" Simple is better than complex")
+print("Namespaces are one honking great idea -- let's do more of those!")
